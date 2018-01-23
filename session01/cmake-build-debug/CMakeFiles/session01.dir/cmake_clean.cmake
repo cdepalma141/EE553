5e6584b6c1257911ddff0c748d1e090b71a2fc35
Connor DePalma <connordepalma@host-b430-111.dhcp.stevens-tech.edu>
2018-01-22 19:25:55 -0500

    first 'Hello World'

---
+++ session01/cmake-build-debug/CMakeFiles/session01.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/session01.dir/main.cpp.o"
+  "session01.pdb"
+  "session01"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/session01.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ session01/main.cpp
@@ -0,0 +1,11 @@
+// .cpp .cxx .cc
+// g++ --std=c++11 yourprogram.cc
+
+#include <iostream> //preprocessor is #
+
+using namespace std;
+
+int main() {
+    cout << "Hello, World!" << std::endl;
+    return 0;
+}
