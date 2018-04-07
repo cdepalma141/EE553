0238389135b6c23d4ae655fc6706444b7e1b32bc
Connor DePalma <connordepalma@CMP.local>
2018-04-07 19:54:17 -0400

    messed with postscript stuff for hw

--- HW6/Postscript2.cpp
+++ HW6/Postscript2.cpp
@@ -14,7 +14,7 @@ class Postscript {
 private:
     string file;
 
-    ofstream stream;
+    fstream stream;
 
     double r, g, b;
 
@@ -22,18 +22,18 @@ public:
 
     Postscript (string const & file) : file(file) {
 
-        r = g = b = 0;
 
-        stream.open (file, ofstream::out | ofstream::app); //opens stream and allows for each new addition to be appended to the last
 
-        stream << r << ' ' << g << ' ' << ' ' << b << " setrgbcolor\n\n";
+        stream.open (file, ios::trunc | ios::out); //opens stream and allows for each new addition to be appended to the last
+
+
 
     }
 
 
     void line(double x, double y, double x1, double y1) {
 
-        stream << r << " " << g << " " << " " << b << " setrgbcolor\n\n";
+
 
         stream << x << " " << y << " moveto\n" << x1 << " " << y1 << " lineto\nstroke\n\n";
 
@@ -45,11 +45,13 @@ public:
         g = gn;
         b = bl;
 
+          stream << r << " " << g << " " << " " << b << " setrgbcolor\n\n";
+
     }
 
     void drawRect(double x, double y, double w, double h) {
 
-        stream << r << " " << g << " " << " " << b << " setrgbcolor\n\n";
+
 
         stream << x << " " << y << " moveto\n" << x+w << " " << y << " lineto\n"
                << x+w << " " << y+h << " lineto\n" << x << " " << y+h << " lineto\n"
@@ -59,19 +61,36 @@ public:
 
     void fillRect(double x, double y, double w, double h) {
 
-        stream << r << " " << g << " " << " " << b << " setrgbcolor\n\n";
+
 
         stream << x << " " << y << " moveto\n" << x+w << " " << y << " lineto\n"
                << x+w << " " << y+h << " lineto\n" << x << " " << y+h << " lineto\n"
-               << "closepath\nfill\n";
+               << "closepath\nfill\n\n";
 
     }
 
     void drawTriangle(double x1, double y1, double x2, double y2, double x3, double y3) {
 
-        stream << x1 << " " << y1 << " moveto\n";
+        stream << x1 << " " << y1 << " moveto\n"
+        <<x2 << " " << y2 <<" lineto\n"
+        <<x3 << " " << y3 <<" lineto\n"
+        <<"closepath\n stroke\n\n";
 
-        if (x1 < x2 & y1 < y2) {
+}
+
+void fillTriangle(double x1, double y1, double x2, double y2, double x3, double y3) {
+
+    stream << x1 << " " << y1 << " moveto\n"
+    <<x2 << " " << y2 <<" lineto\n"
+    <<x3 << " " << y3 <<" lineto\n"
+    <<"closepath\n fill\n\n";
+
+}
+
+
+
+#if 0
+        if (x1 < x2) {
             stream << x1 + x2 << " " << y1 + y2 << " lineto\n";
         }
         if (x1 > x2 & y1 < y2) {
@@ -83,7 +102,6 @@ public:
         if (x1 < x2 & y1 > y2) {
         stream << x1 + x2 << " " << y1 - y2 << " lineto\n";
         }
-
 
         if(x2 < x3 & y2 < y3)
             stream << x2+x3 << " " << y2+y3 << " lineto\n";
@@ -100,27 +118,14 @@ public:
         stream << "closepath\nstroke\n\n";
 
     }
+    #endif
 
 };
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 int main() {
-    Postscript p("/Users/connordepalma/Desktop/text.ps");
+    Postscript p("text.ps");
     int r = 255, g = 0, b = 0;
 
     p.setColor(r, g, b);
@@ -135,10 +140,14 @@ int main() {
 
     p.fillRect(110, 0, 100, 100);   // x y moveto x+w y lineto x+w y+h lineto ... closepath fill
 
-    p.drawTriangle(300, 0, 400, 0, 400, 500);
+    p.setColor(0, 0, 255);
 
-/*
-    p.fillTriangle(x1,y1, x2,y2, x3,y3);
+    p.drawTriangle(0, 150, 100, 150, 50, 225);
+
+    p.setColor(0, 0, 255);
+
+    p.fillTriangle(220, 0, 320, 0, 270, 75);
+    /*
     p.drawCircle(x,y,r); // x y 0 360 r arc stroke
     p.text(x,y, "testing testing 123"); // look it up  setfont  (ABC) show
 
